1dd41f5f1ed08a514ad95a84cedfab1f79822b44
oOBobbyOo <944831575@qq.com>
2022-09-27 15:33:19 +0800

    feat: useWindowScroll

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,6 +22,7 @@ module.exports = {
   plugins: ['react', 'react-hooks', '@typescript-eslint', 'prettier'],
   rules: {
     'prettier/prettier': 'error',
-    '@typescript-eslint/no-explicit-any': ['off']
+    '@typescript-eslint/no-explicit-any': ['off'],
+    '@typescript-eslint/no-empty-function': 'off'
   }
 }
