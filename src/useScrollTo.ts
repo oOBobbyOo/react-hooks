7868333a3e78b811e41715757c110f79b2397ad8
oOBobbyOo <944831575@qq.com>
2023-03-17 15:51:54 +0800

    feat: useScroolTo

---
+++ src/useScrollTo.ts
@@ -0,0 +1,18 @@
+import { useEffect, useRef, useState } from 'react'
+import type { RefObject } from 'react'
+
+const useScrollTo = <T extends Element>(): [RefObject<T>, (val: boolean) => void] => {
+  const ref = useRef<T>(null)
+  const [shouldScrollTo, setShouldScrollTo] = useState(false)
+
+  useEffect(() => {
+    if (ref.current && shouldScrollTo) {
+      ref.current.scrollIntoView({ behavior: 'smooth' })
+      setShouldScrollTo(false)
+    }
+  }, [shouldScrollTo])
+
+  return [ref, setShouldScrollTo]
+}
+
+export default useScrollTo
