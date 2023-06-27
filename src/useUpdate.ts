46f2221786aaf378746474de6d2114db59542de1
oOBobbyOo <944831575@qq.com>
2023-06-27 16:50:03 +0800

    feat: useUpdate

--- src/useUpdate.ts
+++ src/useUpdate.ts
@@ -8,3 +8,14 @@ const useUpdate = () => {
 }
 
 export default useUpdate
+
+// react-use
+/* import { useReducer } from 'react'
+
+const updateReducer = (num: number): number => (num + 1) % 1_000_000
+
+export function useUpdate(): () => void {
+  const [, update] = useReducer(updateReducer, 0)
+  return update
+}
+*/
