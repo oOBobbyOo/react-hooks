56437e38e9a3fcdc8e57c26c00c007581b1f7c04
oOBobbyOo <944831575@qq.com>
2023-08-03 12:20:27 +0800

    feat: useRem

---
+++ src/useRem.ts
@@ -0,0 +1,26 @@
+import { useCallback, useEffect } from 'react'
+import { debounce } from 'lodash-es'
+
+const useRem = (designWidth: number = 750) => {
+  const setRemFontBaseSize = useCallback(() => {
+    const clientWidth = document.documentElement.clientWidth || document.body.clientWidth
+    const rem = (clientWidth / designWidth) * 100
+    document.documentElement.style.fontSize = rem + 'px'
+  }, [designWidth])
+
+  useEffect(() => {
+    setRemFontBaseSize()
+
+    const debouncedFontSize = debounce(() => {
+      setRemFontBaseSize()
+    }, 100)
+
+    window.addEventListener('resize', debouncedFontSize, false)
+
+    return () => {
+      window.removeEventListener('resize', debouncedFontSize, false)
+    }
+  }, [])
+}
+
+export default useRem
